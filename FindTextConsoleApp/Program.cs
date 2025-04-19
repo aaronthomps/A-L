8ac8c2996d22556554d9c723c35dccb4679dd738
Aaron Thompson <plaids-pagoda.9g@icloud.com>
2025-04-19 11:59:06 -0400

    text search console app

---
+++ FindTextConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TextSearch;
+
+const string SearchDir="enter";
+const string SearchText = "file";
+const string Output="output.txt";
+FileSearch.FindTextInDirectoryAndOutputToFile(SearchDir, SearchText, Output);
